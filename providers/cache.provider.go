ce953092c12eeeb9466b2d3a0dcdcf73f39469cb
newtalentxp <newtalentxp@126.com>
2021-03-28 18:56:14 +0800

    fix

--- providers/cache.provider.go
+++ providers/cache.provider.go
@@ -11,14 +11,14 @@ func NewCacheProvider(config config.Config) cache.Cache {
 	driver := config.Get("cache", "addrs").String("redis")
 	prefix := config.Get("cache", "prefix").StringSlice([]string{""})
 
-	var cache cache.Cache
+	var c cache.Cache
 	switch driver {
 	case "redis":
 		addrs := config.Get("cache", "addrs").StringSlice([]string{":6379"})
-		cache := redis.NewCache(cache.WithPrefix(prefix), redis.WithAddrs(addrs...))
+		c := redis.NewCache(cache.WithPrefix(prefix), redis.WithAddrs(addrs...))
 	default:
-		cache := memory.NewCache(cache.WithPrefix(prefix))
+		c := memory.NewCache(cache.WithPrefix(prefix))
 	}
 
-	return cache
+	return c
 }
